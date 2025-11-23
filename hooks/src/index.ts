cc417ae555193fa75dea37061fab3740fe18fd5f
HardikDudeja <hardik.dudeja39@gmail.com>
2025-11-23 14:16:50 +0530

    feat: implement ZapRun and ZapRunOutbox models to persist trigger data from webhook catches.feat: implement ZapRun and ZapRunOutbox models to persist trigger data from webhook catches.

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,13 +1,32 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
+const client = new PrismaClient();
 const app = express();
 
 // there should be a passoword logic as well so that is not triggered by anyone
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
+  const body = req.body;
 
   // store the trigger in the database
+  await client.$transaction(async (tx: any) => {
+    const run = await tx.zapRun.create({
+      data: {
+        zapId: zapId,
+        metadata: body,
+      },
+    });
 
-  // pust it to the queue kafka or redis or any other queue
+    await tx.zapRunOutbox.create({
+      data: {
+        zapRunId: run.id,
+      },
+    });
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
