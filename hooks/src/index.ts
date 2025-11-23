2f14e9cbcdaaadf3152fb0a259ff0bee955d6935
HardikDudeja <hardik.dudeja39@gmail.com>
2025-11-23 15:06:20 +0530

    feat: Implement webhook endpoint with JSON body parsing, environment variable support, and updated TypeScript configuration.

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,14 +1,18 @@
+import "dotenv/config";
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/client";
 
 const client = new PrismaClient();
 const app = express();
+app.use(express.json());
 
 // there should be a passoword logic as well so that is not triggered by anyone
 app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
   const userId = req.params.userId;
   const zapId = req.params.zapId;
-  const body = req.body;
+  const body = req.body ?? {};
+
+  console.log("printing body", zapId, body)
 
   // store the trigger in the database
   await client.$transaction(async (tx: any) => {
@@ -25,6 +29,9 @@ app.post("/hooks/catch/:userId/:zapId", async (req, res) => {
       },
     });
   });
+  res.json({
+    message: "Webhook received successfully"
+  })
 });
 
 app.listen(3000, () => {
